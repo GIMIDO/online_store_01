a06ad01d5cfdf111213c5c8a89ca61c54485ce16
GIMIDO <55269642+GIMIDO@users.noreply.github.com>
2021-07-26 16:32:37 +0300

    0.2
    Добавлена регистрация и вход. Добавлен профиль пользователя

--- shop/mainapp/views.py
+++ shop/mainapp/views.py
@@ -4,10 +4,11 @@ from django.views.generic import DetailView, View
 from django.http import HttpResponseRedirect
 from django.contrib.contenttypes.models import ContentType
 from django.contrib import messages
+from django.contrib.auth import authenticate, login
 
-from .models import Shoes, Pants, Hoodie, Category, LatestProducts, Client, Cart, CartProduct
+from .models import Shoes, Pants, Hoodie, Category, LatestProducts, Client, Cart, CartProduct, Order
 from .mixins import CategoryDetailMixin, CartMixin
-from .forms import OrderForm
+from .forms import OrderForm, LoginForm, RegistrationForm
 from .utils import recalc_cart
 
 
@@ -150,3 +151,63 @@ class MakeOrderView(CartMixin, View):
             messages.add_message(request, messages.INFO, 'Заказано!')
             return HttpResponseRedirect('/')
         return HttpResponseRedirect('/checkout/')
+
+
+class LoginView(CartMixin, View):
+
+    def get(self, request, *args, **kwargs):
+        form = LoginForm(request.POST or None)
+        categories = Category.objects.all()
+        context = {'form': form, 'categories': categories, 'cart': self.cart}
+        return render(request, 'login.html', context)
+
+    def post(self, request, *args, **kwargs):
+        form = LoginForm(request.POST or None)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+                return HttpResponseRedirect('/')
+        context = {'form': form, 'cart': self.cart}
+        return render(request, 'login.html', context)
+
+
+class RegistrationView(CartMixin, View):
+    def get(self, request, *args, **kwargs):
+        form = RegistrationForm(request.POST or None)
+        categories = Category.objects.all()
+        context = {'form': form, 'categories': categories, 'cart': self.cart}
+        return render(request, 'registration.html', context)
+
+    def post(self, request, *args, **kwargs):
+        form = RegistrationForm(request.POST or None)
+        if form.is_valid():
+            new_user = form.save(commit=False)
+            new_user.username = form.cleaned_data['username']
+            new_user.email = form.cleaned_data['email']
+            new_user.first_name = form.cleaned_data['first_name']
+            new_user.last_name = form.cleaned_data['last_name']
+            new_user.save()
+            new_user.set_password(form.cleaned_data['password'])
+            new_user.save()
+            Client.objects.create(
+                user=new_user,
+                phone=form.cleaned_data['phone'],
+                address=form.cleaned_data['address']
+            )
+            user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
+            login(request, user)
+            return HttpResponseRedirect('/')
+        context = {'form': form, 'cart': self.cart}
+        return render(request, 'registration.html', context)
+
+
+class ProfileView(CartMixin, View):
+
+    def get(self, request, *args, **kwargs):
+        client = Client.objects.get(user=request.user)
+        orders = Order.objects.filter(client=client).order_by('-created_at')
+        categories = Category.objects.all()
+        return render(request, 'profile.html', {'orders': orders, 'cart': self.cart, 'categories': categories})
